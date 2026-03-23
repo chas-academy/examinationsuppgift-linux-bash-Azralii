a1db1fe634e8e876f9f0542f4fe6f509f46b5cb2
Azralii <111491491+Azralii@users.noreply.github.com>
2026-03-23 19:10:20 +0100

    Refactor create_users.sh for improved user creation

--- create_users.sh
+++ create_users.sh
@@ -1,6 +1,4 @@
-
-    #!/bin/bash
-
+#!/bin/bash
 # Kontrollera att scriptet körs som root
 if [ "$EUID" -ne 0 ]; then
     echo "This script must be run as root"
@@ -9,19 +7,28 @@ fi
 
 # Skapa användare från argument
 for username in "$@"; do
-    useradd -m -U --badname "$username"
+    useradd -m "$username"
 
     home_dir="/home/$username"
 
-    mkdir -p "$home_dir/Documents" "$home_dir/Downloads" "$home_dir/Work"
+    # Skapa kataloger
+    mkdir -p "$home_dir/Documents"
+    mkdir -p "$home_dir/Downloads"
+    mkdir -p "$home_dir/Work"
+
+    # Sätt ägarskap
     chown -R "$username:$username" "$home_dir"
-    chmod 700 "$home_dir/Documents" "$home_dir/Downloads" "$home_dir/Work"
 
-    {
-        echo "Välkommen $username"
-        awk -F: -v user="$username" '$1 != user { print $1 }' /etc/passwd
-    } > "$home_dir/welcome.txt"
+    # Sätt rättigheter
+    chmod 700 "$home_dir/Documents"
+    chmod 700 "$home_dir/Downloads"
+    chmod 700 "$home_dir/Work"
 
+    # Skapa welcome.txt
+    echo "Välkommen $username" > "$home_dir/welcome.txt"
+    awk -F: '{print $1}' /etc/passwd >> "$home_dir/welcome.txt"
+
+    # Sätt ägare och rättigheter på welcome.txt
     chown "$username:$username" "$home_dir/welcome.txt"
     chmod 600 "$home_dir/welcome.txt"
 done
