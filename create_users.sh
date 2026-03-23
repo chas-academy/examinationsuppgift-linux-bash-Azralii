c799a8a7288a9cd8f0c0a19fceae83ba66c595df
Azralii <111491491+Azralii@users.noreply.github.com>
2026-03-23 16:39:39 +0100

    Refactor user creation script for improved efficiency

--- create_users.sh
+++ create_users.sh
@@ -8,30 +8,28 @@ fi
 
 # Skapa användare från argument
 for username in "$@"; do
-    useradd -m "$username"
+    # Skapa användaren med hemkatalog och egen grupp
+    useradd -m -U --badname "$username"
 
     home_dir="/home/$username"
 
     # Skapa kataloger
-    mkdir -p "$home_dir/Documents"
-    mkdir -p "$home_dir/Downloads"
-    mkdir -p "$home_dir/Work"
+    mkdir -p "$home_dir/Documents" "$home_dir/Downloads" "$home_dir/Work"
 
     # Sätt ägarskap
-    chown "$username:$username" "$home_dir/Documents"
-    chown "$username:$username" "$home_dir/Downloads"
-    chown "$username:$username" "$home_dir/Work"
+    chown -R "$username:$username" "$home_dir"
 
-    # Sätt rättigheter
-    chmod 700 "$home_dir/Documents"
-    chmod 700 "$home_dir/Downloads"
-    chmod 700 "$home_dir/Work"
+    # Sätt rättigheter så bara ägaren kommer åt mapparna
+    chmod 700 "$home_dir/Documents" "$home_dir/Downloads" "$home_dir/Work"
 
     # Skapa welcome.txt
-    echo "Välkommen $username" > "$home_dir/welcome.txt"
-    awk -F: -v user="$username" '$1 != user {print $1}' /etc/passwd >> "$home_dir/welcome.txt"
+    {
+        echo "Välkommen $username"
+        awk -F: -v user="$username" '$1 != user { print $1 }' /etc/passwd
+    } > "$home_dir/welcome.txt"
 
     # Sätt ägare och rättigheter på welcome.txt
     chown "$username:$username" "$home_dir/welcome.txt"
     chmod 600 "$home_dir/welcome.txt"
 done
+
