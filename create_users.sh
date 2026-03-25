970a299de09f28ae0a3236247ce802c01654a932
Liibaan Abdulle <din-jobbmail@effectit.se>
2026-03-25 09:19:07 +0100

    Fix create_users and add video placeholder

--- create_users.sh
+++ create_users.sh
@@ -1,39 +1,36 @@
 #!/bin/bash
 
-# Kontrollera att scriptet körs som root
+# Kontrollera root
 if [ "$EUID" -ne 0 ]; then
     echo "This script must be run as root"
     exit 1
 fi
 
-# Skapa alla användare som skickas in som argument
+# Loop användare
 for username in "$@"; do
     useradd -m "$username" 2>/dev/null
 
     home_dir="/home/$username"
 
-    # Skapa mappar
+    # mappar
     mkdir -p "$home_dir/Documents"
     mkdir -p "$home_dir/Downloads"
     mkdir -p "$home_dir/Work"
 
-    # Sätt ägare
+    # ägare
     chown -R "$username:$username" "$home_dir"
 
-    # Sätt rättigheter
+    # rättigheter
     chmod 700 "$home_dir/Documents"
     chmod 700 "$home_dir/Downloads"
     chmod 700 "$home_dir/Work"
 
-    # Skapa welcome.txt
-    echo "Välkommen $username" > "$home_dir/welcome.txt"
+    # welcome.txt
+    {
+        echo "Välkommen $username"
+        awk -F: -v current="$username" '$1 != current { print $1 }' /etc/passwd
+    } > "$home_dir/welcome.txt"
 
-    # Lägg till alla användare i filen
-    for other_user in "$@"; do
-        echo "$other_user" >> "$home_dir/welcome.txt"
-    done
-
-    # Sätt rättigheter på welcome.txt
     chown "$username:$username" "$home_dir/welcome.txt"
     chmod 600 "$home_dir/welcome.txt"
 done
